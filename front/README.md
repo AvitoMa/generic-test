f8b5f166ab45722b03a0f2baa507da2285d9a055
Hamza Aid <hamza@appsdoc.com>
2018-01-15 01:57:22 +0000

    Add the front-end app

---
+++ front/README.md
@@ -0,0 +1,23 @@
+### Description
+This repository contains a front-end SPA app, its using React to manipulate the DOM, the skelton of the app was generate using : https://github.com/facebookincubator/create-react-app
+
+### How to run Localy
+`npm start` or `yarn start`
+
+### How to build
+`npm run build` or `yarn build`
+This will build the app files in `/build` directory, ready to be dispacted to production
+
+### Dependencies
+* Node >=8.5.0
+* npm 5.2+
+* Factorial Go API
+
+### FYI
+API Endpoint is hardcoded in `src/App.js` Line 5, I don't think this is a good practice, do you ?
+
+### Good to have
+The response of each call is logged to browser console, try to log it in a DOM element once it is received from the API server
+
+### Deployment
+This application needs to be deployed behind a web server, preferred Nginx
---
+++ front/src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import './App.css';
+
+const API_URL = "http://localhost:8080/"
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '5'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = this.state.value.split("\n");
+    data.forEach(function(number) {
+      getFactorial(number);
+  });
+  }
+
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <FormGroup>
+          <Label for="factorialText">Input one number per line</Label>
+          <Input type="textarea" name="factorialText" value={this.state.value} onChange={this.handleChange} />
+        </FormGroup>
+        <Button>Calculate Factorial</Button>
+      </Form>
+    );
+  }
+}
+
+function getFactorial(num) {
+  return fetch(API_URL + num)
+    .then((response) => {
+      console.log('Factorial of ' + num + ' is ' + response);
+      return response;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+export default App;
